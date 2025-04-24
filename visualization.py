4eaec147d5237dc5697114dac11aa392851e1997
Vishal Gorule <gorulevishal984@gmail.com>
2025-04-24 06:40:02 +0530

    utils updated

--- visualization.py
+++
@@ -1,2 +0,0 @@
-# This file has been moved to src/scripts/visualization.py
-# Please use the script from src/scripts/visualization.py
--- web_app.py
+++
@@ -1,2 +0,0 @@
-# This file has been moved to src/scripts/web_app.py
-# Please use the script from src/scripts/web_app.py
